0ee5e4ecb5245b21e08d22e5966efaed70a971db
marqzxq <77423720+marqzxq@users.noreply.github.com>
2021-01-26 03:07:40 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,285 @@
+const help = (prefix) => {
+	return `
+
+➣Olá, esses são meus comandos, não se esqueça de me seguir no insta e divulgar o número para os amigos <3  
+
+https://instagram.com/marquin.xz?igshid=dl4lbvg5ie7d
+
+〲
+〲┴┬➣𝐃𝐎𝐍𝐎: 𝐌𝐀𝐑𝐐𝐔𝐈𝐍7 愛
+〲
+〲┴┬➣ wa.me/5511977782073
+〲
+〲┴┬➣𝐒𝐓𝐀𝐓𝐔𝐒: ON
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲╭─────⊣〘 𝗙𝗜𝗚𝗨𝗥𝗜𝗡𝗛𝗔 〙
+〲│
+〲┴┬➣ COMANDO: '.sticker'
+〲┴┬➣ DESC: Converter em figurinha
+〲┴┬➣ USO: Converter em figurinha
+〲
+〲┴┬➣ COMANDO: '.sticker nobg'
+〲┴┬➣ DESC: Imagem em figurinha sem fundo
+〲┴┬➣ USO: Enviar imagem com legenda
+〲
+〲┴┬➣ COMANDO: '.toimg'
+〲┴┬➣ DESC: Converter figurinha em imagem
+〲┴┬➣ USO: Figurinha de resposta
+〲
+〲┴┬➣ COMANDO: '.tsticker'
+〲┴┬➣ DESC: Converter texto em Figurinha
+〲┴┬➣ USO: .tsticker [seu texto]
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗠𝗘𝗠𝗘 〙
+〲│
+〲┴┬➣ COMANDO: '.meme'
+〲┴┬➣ DESC: Imagens aleatórias de meme
+〲┴┬➣ USO: Apenas envie o comando
+〲
+〲┴┬➣ COMANDO: '.memeindo'
+〲┴┬➣ DESC: Imagens aleatórias de meme
+〲┴┬➣ USO: Apenas envie o comando
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗢𝗨𝗧𝗥𝗢𝗦 〙 
+〲│
+〲┴┬➣ COMANDO: '.gtts'
+〲┴┬➣ DESC: Fazer o bot gravar áudio
+〲┴┬➣ USO: .gtts [cc] [text]
+〲┴┬➣ EXEMPLO: .gtts ja On2-chan
+〲
+〲┴┬➣ COMANDO: '.url2img'
+〲┴┬➣ DESC: Tirar screenshots da web
+〲┴┬➣ USO: .url2img [tipe] [url]
+〲
+〲┴┬➣ COMANDO: '.simi'
+〲┴┬➣ DESC: Sua mensagem será respondida
+〲┴┬➣ USO: .simi [sua mensagem]
+〲
+〲┴┬➣ COMANDO: '.ocr'
+〲┴┬➣ DESC: Pegue o texto na foto
+〲┴┬➣ USO: Enviar imagem com legenda
+〲
+〲┴┬➣ COMANDO: '.wait'
+〲┴┬➣ DESC: Procure anime [Que anime é]
+〲┴┬➣ USO: Enviar imagem com legenda
+〲
+〲┴┬➣ COMANDO: '.clone'
+〲┴┬➣ DESC: Usar foto de perfil da pessoa
+〲┴┬➣ USO: .clone @member
+〲┴┬➣ NOTA: Usado por adms
+〲
+〲┴┬➣COMANDO:  '.db'
+〲┴┬➣DESC: Envia link com database de travazap
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '.wppim'
+〲┴┬➣DESC: Envia link de Whatsapp imunes
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '.nethunter'
+〲┴┬➣DESC: Tutorial de kali linux no seu termux
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣ COMANDO: '.listaadmins'
+〲┴┬➣ DESC: Marca todos os adms do grupo
+〲┴┬➣ USO: .listaadmins
+〲┴┬➣ NOTA: Usado por membros do grupo
+〲
+〲┴┬➣ COMANDO: '.nulis'
+〲┴┬➣ DESC: Foto com seu texto no caderno
+〲┴┬➣ USO: .nulis [texto]
+〲┴┬➣ NOTA: Usado em grupos
+〲
+〲┴┬➣ COMANDO: '.signome'
+〲┴┬➣ DESC: Mostra o significado do seu nome
+〲┴┬➣ USO:  '.signome [nome]'
+〲
+〲┴┬➣ COMANDO: '.blocklist'
+〲┴┬➣ DESC Lista de ctts bloqueados
+〲┴┬➣ USO: .blocklist
+〲┴┬➣ NOTA: Usado em grupos
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 +18 〙
+〲│
+〲┴┬➣COMANDO:  *.nekoanime*
+〲┴┬➣DESC: Imagens aleatórias de neko
+〲┴┬➣USO: Apenas envie o comando
+〲
+〲┴┬➣ COMANDO: '.loli'
+〲┴┬➣ DESC: Imagens de loli
+〲┴┬➣ USO: Apenas envie o comando
+〲
+〲┴┬➣COMANDO: '.randomanime-
+〲┴┬➣DESC: Imagens aleatórias de anime
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO: '.randomhentai'
+〲┴┬➣DESC: Imagens aleatórias de hentai
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO: '.nsfwblowjob'
+〲┴┬➣DESC: Gifs de blowsjob de hentai
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COAMANDO:  '.nsfwneko'
+〲┴┬➣DESC: Imagens de hentai neko
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '.nsfwtrap'
+〲┴┬➣DESC: Imagens de hentai trap
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '.nsfwloli'
+〲┴┬➣DESC: Imagens de hentai
+〲┴┬➣USO: Apenas envie o comando 
+〲┴┬➣NOTA: Funciona somente no pv do bot, ou se for adm
+〲
+〲┴┬➣COMANDO:  '.shota'
+〲┴┬➣DESC: Imagens aleatórias de shota (loli masculinas)
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '.pack'
+〲┴┬➣DESC: Pacotes de links +18
+〲┴┬➣USO: Apenas envie o comando
+〲
+〲┴┬➣COMANDO:  '.nsfw'
+〲┴┬➣DESC: Ativar o modo hentai no grupo 
+〲┴┬➣USO:  '.nsfw' 1/0 ativar e desativar 
+〲┴┬➣OBS: Este comando só pode ser usado se você for adm
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗚𝗥𝗨𝗣𝗢 〙
+〲│
+〲┴┬➣ COMANDO: '.add'
+〲┴┬➣ DESC: Adicionar membro ao grupo
+〲┴┬➣ USO: .add 55xxxxxxxx
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '.kick'
+〲┴┬➣ DESC: Banir membros do grupo
+〲┴┬➣ USO: .kick @member
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '.promote'
+〲┴┬➣ DESC: Torna um membro como adm 
+〲┴┬➣ USO: .promote @member
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '.demote'
+〲┴┬➣ DESC: Tornar o um adm como membro
+〲┴┬➣ USO: .demote @member
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '.linkgroup'
+〲┴┬➣ DESC: Enviar o link do grupo
+〲┴┬➣ USO: Apenas envie o comando
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '.simih'
+〲┴┬➣ DESC: Ative o modo simi no grupo
+〲┴┬➣ ATIVAR: .simih 1 
+〲┴┬➣ DESATIVAR: .simih 0
+〲┴┬➣ NOTA: Usado quando você é adm
+〲
+〲┴┬➣ COMANDO: '.welcome'
+〲┴┬➣ DESC: Ative o recurso de boas vindas
+〲┴┬➣ ATIVAR: .welcome 1 
+〲┴┬➣ DESATIVAR: .welcome 0
+〲┴┬➣ NOTA: Usado quando o bot é adm!
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 𝗗𝗘 𝗧𝗘𝗫𝗧𝗢 〙
+〲│
+〲┴┬➣COMANDO:  '.frase'
+〲┴┬➣DESC: Envia uma frase filosófica 
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '.textph'
+〲┴┬➣DESC: Converte texto na logo do pornhub
+〲┴┬➣USO:  '.textph [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '.traduz'
+〲┴┬➣DESC: Traduz seu texto em outra língua
+〲┴┬➣USO:  '.traduz [pt] [texto]'
+〲
+〲┴┬➣COMANDO:  '.idioma'
+〲┴┬➣DESC: Lista todos os códigos de idiomas
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '.text3d'
+〲┴┬➣DESC: Converte o texto em imagem 3d
+〲┴┬➣USO:  '.text3d [pt1]'
+〲
+〲┴┬➣COMANDO:  '.wolflogo'
+〲┴┬➣DESC: Converte o texto em imagem com logo de um lobo
+〲┴┬➣USO:  '.wolflogo [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '.lionlogo'
+〲┴┬➣DESC: Converte texto em imagem com logo de um leao
+〲┴┬➣USO:  '.lionlogo [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '.glitch'
+〲┴┬➣DESC: Converte texto em imagem com estilo glitch
+〲┴┬➣USO:  '.glitch [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '.ninjalogo'
+〲┴┬➣DESC: Converte texto em imagem com logo de um ninja
+〲┴┬➣USO:  '.ninjalogo [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '.neonligth'
+〲┴┬➣DESC: Converte texto em imagem com estilo neon ligth
+〲┴┬➣USO:  '.neonligth [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '.snow'
+〲┴┬➣DESC: Converter texto em imagem com estilo snow
+〲┴┬➣USO:  '.snow [pt1]'
+〲
+〲┴┬➣COMANDO:  '.blood'
+〲┴┬➣DESC: Converter texto em imagem com estilo blood
+〲┴┬➣USO  '.blood [pt1]'
+〲
+〲┴┬➣COMANDO:  '.neontec'
+〲┴┬➣DESC: Converter texto em imagem com estilo tecnology
+〲┴┬➣USO:  '.neontec [pt1]'
+〲
+〲┴┬➣COMANDO:  '.dropwater'
+〲┴┬➣DESC: Converter texto em imagem com estilo dropwater
+〲┴┬➣USO:  '.dropwater [pt1]'
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭────⊣〘 𝗘𝗫𝗧𝗥𝗔𝗦 〙
+〲│
+〲┴┬➣ COMANDO:  '.termux'
+〲┴┬➣ DESC: Mostra uma lista com comandos do termux
+〲┴┬➣ USO: Apenas envie o comando 
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲╭────⊣〘 𝗦𝗨𝗣𝗢𝗥𝗧𝗘 〙
+〲│
+〲┴┬➣  wa.me/5511977782073
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲 
+〲 ║▌│█║▌│ █║▌│█│║▌║
+〲 ║▌│█║▌│ █║▌│█│║▌║
+〲
+〲 Copyright © 𝐌𝐀𝐑𝐐𝐔𝐈𝐍7 愛 2021
+〲
+︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+`
+}
+
+exports.help = help
+
+
+
+
+
